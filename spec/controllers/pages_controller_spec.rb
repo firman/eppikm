42c65a1a00fd40d22c629531ab0720dbb40e38a9
firman m <firman.alqoumy@gmail.com>
2010-02-12 15:02:58 +0700

    static pages selesai

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe PagesController do
+integrate_views
+
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                                "E-PPIKM | Home")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                                "E-PPIKM | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title",
+                                "E-PPIKM | Help")
+    end
+  end
+end
