8b6ccba1a49b5402e6f677586495b6dd4964ef9e
firman m <firman.alqoumy@gmail.com>
2010-02-17 22:32:36 +0700

    slese cart dan ajax1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  session :session_key => '_depot_session_id'
+
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery :secret => '7tWDI95y4WTOn4oGQJKSrxkUpJmldD60wexlJu1XvYo'
+  # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
+
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,37 @@
 class StoreController < ApplicationController
+
   def index
-    @title = "Katalog"
+    @title = "Katalog Produk"
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
+
+  def add_to_cart
+  begin
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+    redirect_to_index
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Salah produk")
+  end
+end
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Keranjang belanjanya sekarang kosong")
+  end
+
+private
+
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    redirect_to :action => 'index'
+  end
+
+ def find_cart
+  session[:cart] ||= Cart.new
+ end
 
 end
 
