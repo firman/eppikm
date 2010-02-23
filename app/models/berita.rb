97881034df481c9a1084f527038e2ea7b4441c11
firman m <firman.alqoumy@gmail.com>
2010-02-23 11:28:33 +0700

    news beres2

--- app/models/berita.rb
+++ app/models/berita.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+# Schema version: 20100223015430
+#
+# Table name: beritas
+#
+#  id         :integer(4)      not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Berita < ActiveRecord::Base
+  validates_presence_of :title, :content
+  validates_uniqueness_of :title
 end
+
