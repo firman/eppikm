7f016f8307c8f91ca3a79bf0b205eb09df2171d1
firman m <firman.alqoumy@gmail.com>
2010-02-23 21:18:23 +0700

    tag beres

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100223015430) do
+ActiveRecord::Schema.define(:version => 20100223134948) do
 
   create_table "beritas", :force => true do |t|
     t.string   "title"
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(:version => 20100223015430) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
