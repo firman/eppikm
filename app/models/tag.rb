4b4af46fac5ffa423961da5bcf258514d580c0a4
firman m <firman.alqoumy@gmail.com>
2010-02-23 21:20:20 +0700

    tag beres2

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20100223134948
+#
+# Table name: tags
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  product_id :integer(4)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Tag < ActiveRecord::Base
   belongs_to :product
 end
