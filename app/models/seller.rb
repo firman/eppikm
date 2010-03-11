7085b01890b15bbcb4fe806f41cde00715a5af24
firman m <firman.alqoumy@gmail.com>
2010-03-11 20:59:14 +0700

    session-role blm beresjuga2

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20100225035944
+# Schema version: 20100310231526
 #
 # Table name: sellers
 #
--- test/unit/helpers/sesi_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SesiHelperTest < ActionView::TestCase
-end
