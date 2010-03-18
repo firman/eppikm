8b802eb027c97e2b9c519964ce8f74e6f2daa76c
firman m <firman.alqoumy@gmail.com>
2010-03-18 10:55:29 +0700

    bwt heroku2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 # == Schema Information
-# Schema version: 20100310231526
+# Schema version: 20100316125409
 #
 # Table name: users
 #
-#  id              :integer(4)      not null, primary key
-#  name            :string(255)
-#  email           :string(255)
-#  alamat          :text
-#  hashed_password :string(255)
-#  salt            :string(255)
-#  created_at      :datetime
-#  updated_at      :datetime
+#  id                :integer(4)      not null, primary key
+#  username          :string(255)
+#  email             :string(255)
+#  crypted_password  :string(255)
+#  password_salt     :string(255)
+#  persistence_token :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
 #
 
 require 'spec_helper'
